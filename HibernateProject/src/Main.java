2d401d74fbaff3d2974bbac2d21dd3ac4b53fe2d
Alicja <Alicja@192.168.218.154>
2013-04-06 18:26:55 +0200

    orders

--- HibernateProject/src/Main.java
+++ HibernateProject/src/Main.java
@@ -1,33 +1,36 @@
 import java.util.*;
 import org.hibernate.*;
 import org.hibernate.cfg.*;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 
-@SuppressWarnings("deprecation")
 public class Main {
 	
 	private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
 	static {
 		try {
-			sessionFactory = new Configuration().configure().buildSessionFactory();
-		} catch (Throwable ex) {
-			throw new ExceptionInInitializerError(ex);
+            Configuration configuration = new Configuration().configure();
+
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+           
+		} catch (HibernateException he) {
+			throw new ExceptionInInitializerError(he);
 		}
 	}
 
-	public static SessionFactory getSessionFactory() {
-
-		return sessionFactory;
-	}
-
-	public static void shutdown() {
-
-		getSessionFactory().close();
-	}
 	
 	public static void main(String[] args) {
 
-		Session newSession = getSessionFactory().openSession();
+		getOrders();
+	}
+	
+	static void getEmployees()
+	{
+		Session newSession = sessionFactory.openSession();
 		Transaction newTransaction = newSession.beginTransaction();
 		List<Employee> employeesList = newSession.createSQLQuery(
 				"select * from Employees").addEntity(Employee.class).list();
@@ -41,6 +44,27 @@ public class Main {
 		newTransaction.commit();
 		newSession.close();
 
-		shutdown();
+		sessionFactory.close();
 	}
+	
+	static void getOrders()
+	{
+		Session newSession = sessionFactory.openSession();
+		Transaction newTransaction = newSession.beginTransaction();
+		List<Order> ordersList = newSession.createSQLQuery(
+				"select * from Orders").addEntity(Order.class).list();
+		System.out.println(ordersList.size() + " message(s) found:");
+
+		for (Iterator<Order> iter = ordersList.iterator(); iter.hasNext();) {
+			Order order = (Order) iter.next();
+			Employee empl = order.getEmployeeID();
+			Customer cust = order.getCustomerID();
+			System.out.println(order.getOrderID() + " " + empl.getFirstName() + " " + cust.getCompanyName());
+		}
+		newTransaction.commit();
+		newSession.close();
+
+		sessionFactory.close();
+	}
+	
 }
