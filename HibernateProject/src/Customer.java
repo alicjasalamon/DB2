b5bb9cf11f74e01b583e885fb09e4cf8b34d3ea5
Alicja <Alicja@192.168.218.154>
2013-04-06 18:27:21 +0200

    orders

---
+++ HibernateProject/src/Customer.java
@@ -0,0 +1,19 @@
+
+public class Customer {
+	
+	int customerID;
+	String companyName;
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+}
---
+++ HibernateProject/src/Order.java
@@ -0,0 +1,28 @@
+
+public class Order {
+
+	int orderID;
+	Employee employeeID;
+	Customer customerID;
+	
+	public int getOrderID() {
+		return orderID;
+	}
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+	public Employee getEmployeeID() {
+		return employeeID;
+	}
+	public void setEmployeeID(Employee employeeID) {
+		this.employeeID = employeeID;
+	}
+	public Customer getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(Customer customerID) {
+		this.customerID = customerID;
+	}
+
+
+}
